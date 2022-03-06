681f878a49fcf48931a02276178399c121d07009
Wciv <sanchezeguiajon@gmail.com>
2022-03-06 21:05:43 +0100

    reverse alphabet

---
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,27 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+  ▄   ▄
+  █▀█▀█     * File_name : ft_print_reverse_alphabet.c
+  █▄█▄█
+   ███  ▄▄  * Creation Date : 2022/03/6 @ 18:34:33
+   ████▐█ █
+   ████   █ * Last Modified : 2022/03/3 @ 18:37:02
+   ▀▀▀▀▀▀▀
+* Code : xAthaz <xathaz@protonmail.com>  
+	(ascii from fsymbols.com)
+_._._._._._._._._._._._._._._._._._._._._._._.*/
+
+void	ft_putchar(char	c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letter;
+	
+	letter = 122;
+	do{
+		ft_putchar(letter);
+		letter = letter - 1;
+	}while(letter > 96);
+}
