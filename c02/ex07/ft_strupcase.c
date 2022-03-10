d0d2e1969d35d7868e82c25395acbd11da55b707
Wciv <sanchezeguiajon@gmail.com>
2022-03-10 22:54:58 +0100

    v2 bugfix

--- c02/ex07/ft_strupcase.c
+++ c02/ex07/ft_strupcase.c
@@ -4,7 +4,7 @@
   █▄█▄█
    ███  ▄▄  * Creation Date : 2022/03/10 @ 22:45:53
    ████▐█ █
-   ████   █ * Last Modified : 2022/03/10 @ 22:50:03
+   ████   █ * Last Modified : 2022/03/10 @ 22:54:32
    ▀▀▀▀▀▀▀
 * Code : xAthaz <xathaz@protonmail.com>  
 	(ascii from fsymbols.com)
@@ -17,7 +17,7 @@ char	*ft_strupcase(char *str)
 	i = 0;
 	do
 	{
-		if(str[i] > 96 || str[i] < 123)
+		if(str[i] > 96 && str[i] < 123)
 			str[i] = str[i] - 32;
 		i++;
 	}while(str[i] != '\0');
