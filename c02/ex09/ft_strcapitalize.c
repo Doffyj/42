142816080f73cd45af5c7b38ab42c592980c7169
Wciv <sanchezeguiajon@gmail.com>
2022-03-10 23:14:42 +0100

    v1 bf

--- c02/ex09/ft_strcapitalize.c
+++ c02/ex09/ft_strcapitalize.c
@@ -1,6 +1,6 @@
 /* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
   ▄   ▄
-  █▀█▀█     * File_name : ft_strupcase.c
+  █▀█▀█     * File_name : ft_strcapitalize.c
   █▄█▄█
    ███  ▄▄  * Creation Date : 2022/03/10 @ 23:05:18
    ████▐█ █
@@ -17,9 +17,9 @@ char	*ft_strcapitalize(char *str)
 	i = 0;
 	do
 	{
-		if( i = 0 && (str[i] > 96 && str[i] < 123)
+		if( i = 0 && (str[i] > 96 && str[i] < 123))
 				str[0] = str[0] - 32;
-		else if(str[i-1] = ' ' && (str[i] > 96 && str[i] < 123)
+		else if(str[i-1] = ' ' && (str[i] > 96 && str[i] < 123))
 			str[i] = str[i] -32;
 		i++;
 	}while(str[i] != '\0);
