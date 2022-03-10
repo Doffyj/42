5ddc7c8046ddffb599e9873363e7e450a4835cb2
Wciv <sanchezeguiajon@gmail.com>
2022-03-10 22:50:34 +0100

    v1

---
+++ c02/ex07/ft_strupcase.c
@@ -0,0 +1,25 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+  ▄   ▄
+  █▀█▀█     * File_name : ft_strupcase.c
+  █▄█▄█
+   ███  ▄▄  * Creation Date : 2022/03/10 @ 22:45:53
+   ████▐█ █
+   ████   █ * Last Modified : 2022/03/10 @ 22:50:03
+   ▀▀▀▀▀▀▀
+* Code : xAthaz <xathaz@protonmail.com>  
+	(ascii from fsymbols.com)
+_._._._._._._._._._._._._._._._._._._._._._._.*/
+
+char	*ft_strupcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	do
+	{
+		if(str[i] > 96 || str[i] < 123)
+			str[i] = str[i] - 32;
+		i++;
+	}while(str[i] != '\0');
+	return(str);
+}
