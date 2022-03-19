17cb0f4118d666ec8c0a4350c656b053faab0ae8
Wciv <sanchezeguiajon@gmail.com>
2022-03-19 23:47:32 +0100

    v1

---
+++ c03/ex00/ft_strcmp.c
@@ -0,0 +1,21 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+  ▄   ▄
+  █▀█▀█     * File_name : ft_strcmp.c
+  █▄█▄█
+   ███  ▄▄  * Creation Date : 2022/03/19 @ 23:43:34
+   ████▐█ █
+   ████   █ * Last Modified : 2022/03/19 @ 23:47:12
+   ▀▀▀▀▀▀▀
+* Code : xAthaz <xathaz@protonmail.com>  
+	(ascii from fsymbols.com)
+_._._._._._._._._._._._._._._._._._._._._._._.*/
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	do{
+		i++;
+	}while(s1[i] == s2[i] && s1 != '\0' && s2 != '\0');
+}
