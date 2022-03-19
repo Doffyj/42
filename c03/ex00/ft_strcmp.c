75ffe3fd2f63191f5df007f4af2ebc625cc298d6
Wciv <sanchezeguiajon@gmail.com>
2022-03-19 23:55:22 +0100

    v2

--- c03/ex00/ft_strcmp.c
+++ c03/ex00/ft_strcmp.c
@@ -15,8 +15,7 @@ int	ft_strcmp(char *s1, char *s2)
 	int	i;
 
 	i = 0;
-	do{
+	while(s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0')
 		i++;
-	}while(s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0');
 	return (s1[i] - s2[i]);
 }
